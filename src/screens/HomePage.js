5f69cebc0f387b529e1758709cf6957ca1b901b5
KrystianKierklo <krystian.kierklo3@gmail.com>
2023-06-06 10:54:52 +0200

    toast add

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { pokemonsList } from "../actions/pokemonsActions";
 import PokemonCard from "../components/Card";
 import { Button, Row, Col } from "react-bootstrap";
+import {Toaster, toast} from 'react-hot-toast'
 
 function HomePage() {
 	const dispatch = useDispatch();
@@ -11,12 +12,15 @@ function HomePage() {
 	const pokemonsListState = useSelector((state) => state.pokemonsList);
 	const { pokemons } = pokemonsListState;
 
+	
 	useEffect(() => {
 		dispatch(pokemonsList());
+		toast.success('Witaj na naszej stronie')
 	}, [dispatch]);
 
 	return (
 		<div className="text-4xl text-center underline mt-5">
+			<div><Toaster/></div>
 			<h1>Lista naszych pacjentów: </h1>
 			<a className="my-3" href="/admin/pokomonsList"><button className="my-3 py-2 przycisk">Szczegóły pacjentów</button></a>
 			<a className="my-3 ml-16" href="/locations"><button className="my-3 py-2 przycisk">Lokalizacje</button></a>
